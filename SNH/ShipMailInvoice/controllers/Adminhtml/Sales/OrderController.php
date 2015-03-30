e35b821aef65bd9abb263bf859104cf9deda49b5
seansan <seansan@users.noreply.github.com>
2015-03-30 19:56:19 +0200

    Update OrderController.php

--- SNH/ShipMailInvoice/controllers/Adminhtml/Sales/OrderController.php
+++ SNH/ShipMailInvoice/controllers/Adminhtml/Sales/OrderController.php
@@ -51,10 +51,10 @@ public function _shipmailinvoice($email=true, $ship=true) {
 		foreach ($orderIds as $orderId) {
 			if (!$ship) continue;
 			$order = Mage::getModel('sales/order')->load($orderId);
-			if ($this->isAlreadyShipped($order)) {
-				$this->_getSession()->addNotice(Mage::helper('sales')->__('Order %s has already been shipped (completely).', $orderId));
-				continue;
-			}
+			//if ($this->isAlreadyShipped($order)) {
+			//	$this->_getSession()->addNotice(Mage::helper('sales')->__('Order %s has already been shipped (completely).', $orderId));
+			//	continue;
+			//}
 			//$order = Mage::getModel('sales/order')->loadByIncrementId($orderId);
 			//$itemQty = (int)$order->getItemsCollection()->count();
 			//$shipment = $order->prepareShipment($itemQty);
